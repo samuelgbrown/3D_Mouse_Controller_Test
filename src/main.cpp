5eaf11a7bdf3c1fd6b6f230e7a095062f94fb37c
samuelgbrown <sambrown@optonline.net>
2025-03-11 14:11:19 -0400

    Switched back to nonrmal HX711 library.  Finished toy example, found oscillatory mode (perhaps due to low force sensor sample rate).

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <NBHX711.h>
+// #include <NBHX711.h>
+#include <HX711.h>
 #include <Servo.h>
 #include <limits.h>
 
@@ -29,35 +30,31 @@ constexpr float REVERSE = -1.0f;
 // TODO List:
 // 1. X Update code: Add reading angle from pot
 //    May want to set as some kind of toy, e.g., rotation of the pot sets the set-point of the servo
-// 2. X Set up hardware:
-//    1. X Breadboard: Connect servo, HX711 + load cell, pot
-//    2. X Mount: Should a mount of some kind be printed for this?
-// 3. Test
-//    1. X Test the limToSetPoint flag
+
 //    2. Test the joint design (components feel secure, have full range of motion, little to no backlash)
 //    3. Check scale filter designs - sliding window adequate, or is LP IIR better?
 // 4. Update code: Add capability of measuring all 3 load cells and pots
 // 5. Print out and assemble full joint
 //      May need a mount of some kind - should be pretty solid so there's no backlash!
-
+  
 // Set servo parameters
 constexpr float SERVO_MIN_ANGLE = 0.0f;
 constexpr float SERVO_MAX_ANGLE = 180.0f;
 constexpr float SERVO_MIN_US = 500.0f;
 constexpr float SERVO_MAX_US = 2500.0f;
 constexpr float SERVO_START_POINT = 90.0f;
-constexpr float MAX_SPEED = 25.0f; // degree/s
-constexpr unsigned long SERVO_UPDATE_PERIOD_US = 0;
+constexpr float MAX_SPEED = 50.0f; // degree/s
+constexpr unsigned long SERVO_UPDATE_PERIOD_US = 40000; // 25Hz
 static float CurJointAngle = SERVO_START_POINT;
+static unsigned long LastServoUpdateTime = 0;
 static float ThresholdPoint;
 static Servo servo;
 
 // Set combined force and rotation measurement parameters
-constexpr unsigned long MEASUREMENT_PERIOD_US = 100000; // Assume regular 10Hz measurements
 constexpr unsigned long START_DELAY = 2000; // Start delay, in ms
 // Set Scale parameters
-constexpr float FORCE_SPEED_SCALE = 0.2;   // g/(degree/s)
-constexpr FILTERING_TYPE FILTERING_TO_USE = NO_FILTERING; // Default - use sliding window filter
+constexpr float FORCE_SPEED_SCALE = 0.2;   // g/(degree/s)filter
+constexpr FILTERING_TYPE FILTERING_TO_USE = NO_FILTERING; // Default - use sliding window 
 constexpr byte SCALE_SLIDING_WINDOW_LEN = 10;
 constexpr float SCALE_VAL_TO_GRAMS = 353.8f;
 constexpr byte NUM_TARE_READS = 10;
@@ -98,7 +95,8 @@ constexpr int IIR_IND_1 = 0; // y0 is new output value, x0 is new input val - ne
 constexpr byte ONE_SAMPLE = 1;
 static float IIRInputs[SCALE_IIR_LEN - 1] = {0.0f};
 static float IIROutputs[SCALE_IIR_LEN - 1] = {0.0f};
-static NBHX711 scale( HX711_DOUT_PIN, HX711_SCK_PIN, SCALE_SIZE_BUFFER );
+// static NBHX711 scale( HX711_DOUT_PIN, HX711_SCK_PIN, SCALE_SIZE_BUFFER );
+static HX711 scale;
 static unsigned long LastTareTime = 0;
 static float FiltedRawInputForce = 0.0f;
 static float AdjustedInputForce = 0.0f;
@@ -108,6 +106,9 @@ inline int angleToMicroSeconds( float commandAngle );
 inline float PerformScaleLPIIRFilter( float input );
 void TareScale( void );
 void ResetScaleIIRFilter( void );
+void UpdateTareCheck( unsigned long thisTime );
+void UpdateSensors( unsigned long thisTime );
+void UpdateServo( unsigned long thisTime );
 
 // Combined force and rotation measurement variables
 unsigned long LastMeasureTime = 0;
@@ -123,7 +124,7 @@ constexpr float FORCE_DEAD_ZONE = 20.0;  // g
 // Threshold testing parameters
 // constexpr int SERVO_SET_POINT = 90;                 // degrees
 // constexpr float SERVO_SET_POINT_F = 90.0f;          // degrees
-constexpr float THRESHOLD_FORCE = 100.0f;              // g
+constexpr float THRESHOLD_FORCE = 200.0f;              // g
 constexpr bool THRESHOLD_ONLY_RESISTIVE = false;       // true = Servo cannot move on its own
 constexpr bool THRESHOLD_DIRECTION = THRESHOLD_PUSH;
 
@@ -131,9 +132,6 @@ constexpr bool THRESHOLD_DIRECTION = THRESHOLD_PUSH;
 constexpr float ANALOG_READ_MIN_F = 0.0f;
 constexpr float ANALOG_READ_MAX_F = 1023.0f;
 constexpr float MICROSECONDS_TO_SECONDS =  1000000.0f; // Divide
-
-// Timing variables
-static unsigned long LastTime = micros( );
 
 #ifdef TESTING
 constexpr unsigned long UPDATE_CHECK_PERIOD_US = 300000;
@@ -150,12 +148,9 @@ void setup( )
   #endif
 
   // Initialize the HX711
-  scale.begin( );
-  scale.setScale( SCALE_VAL_TO_GRAMS );
-  while ( !scale.update( ) )
-  {
-    // Wait for scale to be ready
-  }
+  scale.begin( HX711_DOUT_PIN, HX711_SCK_PIN ); // TODO: If still using IIR filter, standard HX711 library is more efficient
+  scale.set_scale( SCALE_VAL_TO_GRAMS );
+  scale.wait_ready( );
   #ifdef TESTING
     Serial.println( "Scale ready!");
   #endif
@@ -176,20 +171,85 @@ void setup( )
 
 void loop( )
 {
-  // Calculate the time difference for this cycle
+  // Get the timestamp for this cycle
   unsigned long thisTime = micros( );
-  float dt; // Time difference in microseconds
-  if ( thisTime >= LastTime )
-  {
-    dt = ( (float) ( thisTime - LastTime ) ) / MICROSECONDS_TO_SECONDS;
-  }
-  else
-  {
-    dt = ( ( (float) thisTime ) + ( (float) ULONG_MAX ) - ( (float) LastTime ) ) /
-      MICROSECONDS_TO_SECONDS ;
-  }
-  LastTime = thisTime;
 
+  UpdateTareCheck( thisTime );
+
+  UpdateSensors( thisTime );
+
+  UpdateServo( thisTime );
+
+  #ifdef TESTING
+  if ( ( thisTime - LastUpdateCheck ) > UPDATE_CHECK_PERIOD_US )
+  {
+    LastUpdateCheck = thisTime;
+    LastNumUpdates = NumUpdates;
+    NumUpdates = 0;
+    Serial.println( );
+    Serial.println( LastNumUpdates );
+    Serial.println( FiltedRawInputForce );
+    Serial.println( ThresholdPoint );
+    Serial.println( CurJointAngle );
+  }
+  #endif
+}
+
+void TareScale( void )
+{
+  // Note: This is a blocking function!
+  // Will block for as long as it takes to get all measurerments!
+  #ifdef TESTING
+  Serial.println( "Do tare!" );
+  Serial.print( "Old offset: " );
+  Serial.println( scale.get_offset( ) );
+  #endif
+  digitalWrite( LED_BUILTIN, HIGH ); // Light LED to indicate TARE is active
+
+  // // Perform a series of fresh updates
+  // byte tareMeasurementsPerformed = 0;
+  // while ( tareMeasurementsPerformed < NUM_TARE_READS )
+  // {
+  //   if ( scale.update( ) )
+  //   {
+  //     tareMeasurementsPerformed++;
+
+  //     #ifdef TESTING
+  //     Serial.print( tareMeasurementsPerformed );
+  //     Serial.print( "-" );
+  //     #endif
+  //   }
+  // }
+
+  // Perform the tare, and reset the filter
+  scale.tare( NUM_TARE_READS );
+  ResetScaleIIRFilter( );
+
+  #ifdef TESTING
+  Serial.println( );
+  Serial.println( "Done with tare!" );
+  Serial.print( "New offset: " );
+  Serial.println( scale.get_offset( ) );
+  #endif
+  digitalWrite( LED_BUILTIN, LOW );
+}
+
+void ResetScaleIIRFilter( void )
+{
+  for ( unsigned char i = 0; i < ( SCALE_IIR_LEN - 1 ); i++ )
+  {
+    IIRInputs[i] = 0.0f;
+    IIROutputs[i] = 0.0f;
+  }
+}
+
+inline float fMap( float x, float in_min, float in_max, float out_min, float out_max )
+{
+  return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+}
+
+void UpdateTareCheck( unsigned long thisTime )
+{
   // Tare scale, if requested
   int newTareButtonVal = digitalRead( BUTTON_PIN );
   if ( newTareButtonVal == LOW )
@@ -200,10 +260,14 @@ void loop( )
       LastTareTime = thisTime;
     }
   }
+}
 
-  // Update measurements
-  if ( ( thisTime - LastMeasureTime ) >= MEASUREMENT_PERIOD_US )
+void UpdateSensors( unsigned long thisTime )
+{
+  // Read from potentiometer and scale
+  if ( scale.is_ready( ) )
   {
+    // When new data is available from the scale (~10Hz), perform the measurement step
     // Measurement from potentiometer
     float rawVal = (float) analogRead( POT_PIN ); // Note: "Left" is 0, "Right" is  180
     PotAngle = fMap(
@@ -211,26 +275,24 @@ void loop( )
     ThresholdPoint = PotAngle;
 
     // Measurement from scale
-    scale.update( ); // TODO: If still using IIR filter, standard HX711 library is more efficient
-
     float rawInputForce;
     float scaledRawInputForce;
     switch ( FILTERING_TO_USE )
     {
       case SLIDING_WINDOW:
-        rawInputForce = scale.getUnits( NUM_AVERAGED_SCALE_READS );
+        rawInputForce = scale.get_units( NUM_AVERAGED_SCALE_READS );
         FiltedRawInputForce = ( rawInputForce > 0.0f ) ?
           ( rawInputForce * SCALE_ASYM_GAIN ) : rawInputForce; // Perform asymmetric gain
         break;
       case IIR_FILTERING:
-        rawInputForce = scale.getUnits( ONE_SAMPLE );
+        rawInputForce = scale.get_units( ONE_SAMPLE );
         scaledRawInputForce = ( rawInputForce > 0.0f ) ?
           ( rawInputForce * SCALE_ASYM_GAIN ) : rawInputForce; // Perform asymmetric gain
         FiltedRawInputForce = PerformScaleLPIIRFilter( scaledRawInputForce ); // TODO: Start here: response is still VERY slow - check filter, try running measuremennts only when there is a new update from the scale, etc.
         break;
       default:
         // No filtering
-        rawInputForce = scale.getUnits( ONE_SAMPLE );
+        rawInputForce = scale.get_units( ONE_SAMPLE );
         FiltedRawInputForce = ( rawInputForce > 0.0f ) ?
           ( rawInputForce * SCALE_ASYM_GAIN ) : rawInputForce; // Perform asymmetric gain
         break;
@@ -253,70 +315,74 @@ void loop( )
   #endif
     LastMeasureTime = thisTime;
   }
+}
 
-  float controlSignal = AdjustedInputForce;
-
-  // Use a threshold on the force measurement
-  // Encode threshold direction by using positive or negative of joint angle
-  // and control signal
-  if ( THRESHOLD_DIRECTION == THRESHOLD_PULL )
+void UpdateServo( unsigned long thisTime )
+{
+  if ( ( thisTime - LastServoUpdateTime ) >= SERVO_UPDATE_PERIOD_US )
   {
-    CurJointAngle = CurJointAngle * REVERSE;
-    controlSignal = controlSignal * REVERSE;
-  }
-
-  bool limToSetPoint = false;
-  if ( CurJointAngle >= ThresholdPoint )
-  {
-    // If the servo angle is GTE the set point, apply the threshold rules
-    if ( !THRESHOLD_ONLY_RESISTIVE || controlSignal > 0 )
+    // Calculate dt for this servo update
+    float dt; // Time difference in microseconds
+    if ( thisTime >= LastServoUpdateTime )
     {
-      // If the actuator is allowed to push back on its own OR the input force is pushing farther up, then apply the threshold
-      limToSetPoint = controlSignal >= 0; // Don't allow threshold force to push actuator BELOW threshold on its own input force is positive
-      controlSignal -= THRESHOLD_FORCE; // Threshold force is pushing DOWN
-      if ( THRESHOLD_ONLY_RESISTIVE )
+      dt = ( (float) ( thisTime - LastServoUpdateTime ) ) / MICROSECONDS_TO_SECONDS;
+    }
+    else
+    {
+      dt = ( ( (float) thisTime ) + ( (float) ULONG_MAX ) - ( (float) LastServoUpdateTime ) ) /
+        MICROSECONDS_TO_SECONDS ;
+    }
+
+    float controlSignal = AdjustedInputForce;
+
+    // Use a threshold on the force measurement
+    // Encode threshold direction by using positive or negative of joint angle
+    // and control signal
+    if ( THRESHOLD_DIRECTION == THRESHOLD_PULL )
+    {
+      CurJointAngle = CurJointAngle * REVERSE;
+      controlSignal = controlSignal * REVERSE;
+    }
+
+    bool limToSetPoint = false;
+    if ( CurJointAngle >= ThresholdPoint )
+    {
+      // If the servo angle is GTE the set point, apply the threshold rules
+      if ( !THRESHOLD_ONLY_RESISTIVE || controlSignal > 0 )
       {
-        controlSignal = max( controlSignal, 0.0f ); // Don't let the servo move on its own (min force of +0)
+        // If the actuator is allowed to push back on its own OR the input force is pushing farther up, then apply the threshold
+        limToSetPoint = controlSignal >= 0; // Don't allow threshold force to push actuator BELOW threshold on its own input force is positive
+        controlSignal -= THRESHOLD_FORCE; // Threshold force is pushing DOWN
+        if ( THRESHOLD_ONLY_RESISTIVE )
+        {
+          controlSignal = max( controlSignal, 0.0f ); // Don't let the servo move on its own (min force of +0)
+        }
       }
     }
+
+    if ( THRESHOLD_DIRECTION == THRESHOLD_PULL )
+    {
+      controlSignal = controlSignal * REVERSE;
+    }
+
+    // Convert the force measurement into a velocity
+    float dJointAngleDt = forceSpeedCurve( controlSignal );
+
+    // Integrate the velocity to get the change in position
+    float dJointAngle = dJointAngleDt * dt;
+
+    // Find the new joint angle
+    float minAngle = limToSetPoint ? ThresholdPoint : SERVO_MIN_ANGLE; // TODO START HERE (limToSetPoint fixed)
+    CurJointAngle += dJointAngle;
+    CurJointAngle = constrain( CurJointAngle, minAngle, SERVO_MAX_ANGLE );
+
+    // #ifndef TESTING
+    // Write the new angle
+    servo.writeMicroseconds( angleToMicroSeconds( CurJointAngle ) );
+    // #endif
+
+    LastServoUpdateTime = thisTime;
   }
-
-  if ( THRESHOLD_DIRECTION == THRESHOLD_PULL )
-  {
-    controlSignal = controlSignal * REVERSE;
-  }
-
-  // Convert the force measurement into a velocity
-  float dJointAngleDt = forceSpeedCurve( controlSignal );
-
-  // Integrate the velocity to get the change in position
-  float dJointAngle = dJointAngleDt * dt;
-
-  // Find the new joint angle
-  float minAngle = limToSetPoint ? ThresholdPoint : SERVO_MIN_ANGLE; // TODO START HERE (limToSetPoint fixed)
-  CurJointAngle += dJointAngle;
-  CurJointAngle = constrain( CurJointAngle, minAngle, SERVO_MAX_ANGLE );
-
-#ifdef TESTING
-  if ( ( thisTime - LastUpdateCheck ) > UPDATE_CHECK_PERIOD_US )
-  {
-    LastUpdateCheck = thisTime;
-    LastNumUpdates = NumUpdates;
-    NumUpdates = 0;
-    // Serial.println( LastNumUpdates );
-    Serial.println( );
-    Serial.println( FiltedRawInputForce );
-    Serial.println( controlSignal );
-    Serial.println( ThresholdPoint );
-    Serial.println( dJointAngleDt );
-    Serial.println( CurJointAngle );
-}
-  // Write the value to the servo
-  servo.writeMicroseconds( angleToMicroSeconds( CurJointAngle ) );
-#else
-  // Write the new angle
-  servo.writeMicroseconds( angleToMicroSeconds( CurJointAngle ) );
-#endif
 }
 
 inline float forceSpeedCurve( float forceInput )
@@ -352,57 +418,4 @@ inline float PerformScaleLPIIRFilter( float newInput )
 
   // Return new value
   return newOutput;
-}
-
-void TareScale( void )
-{
-  // Note: This is a blocking function!
-  // Will block for as long as it takes to get all measurerments!
-  #ifdef TESTING
-  Serial.println( "Do tare!" );
-  Serial.print( "Old offset: " );
-  Serial.println( scale.getOffset( ) );
-  #endif
-  digitalWrite( LED_BUILTIN, HIGH ); // Light LED to indicate TARE is active
-
-  // Perform a series of fresh updates
-  byte tareMeasurementsPerformed = 0;
-  while ( tareMeasurementsPerformed < NUM_TARE_READS )
-  {
-    if ( scale.update( ) )
-    {
-      tareMeasurementsPerformed++;
-
-      #ifdef TESTING
-      Serial.print( tareMeasurementsPerformed );
-      Serial.print( "-" );
-      #endif
-    }
-  }
-
-  // Perform the tare, and reset the filter
-  scale.tare( NUM_TARE_READS );
-  ResetScaleIIRFilter( );
-
-  #ifdef TESTING
-  Serial.println( );
-  Serial.println( "Done with tare!" );
-  Serial.print( "New offset: " );
-  Serial.println( scale.getOffset( ) );
-  #endif
-  digitalWrite( LED_BUILTIN, LOW );
-}
-
-void ResetScaleIIRFilter( void )
-{
-  for ( unsigned char i = 0; i < ( SCALE_IIR_LEN - 1 ); i++ )
-  {
-    IIRInputs[i] = 0.0f;
-    IIROutputs[i] = 0.0f;
-  }
-}
-
-inline float fMap( float x, float in_min, float in_max, float out_min, float out_max )
-{
-  return ( x - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
 }
